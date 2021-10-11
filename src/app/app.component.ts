d8c540ddeaf2c4626ba2f86285f5edebcd2bed4d
Aaron Joslin-Wangdu <aaronjoslinwangdu@gmail.com>
2021-10-11 19:47:32 -0400

    inital commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { AuthenticationService } from './service/authentication/authentication.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'autonomous-democracy';
+
+  constructor(public authenticationService: AuthenticationService) {
+
+  }
+
+  logout(){
+    this.authenticationService.logout();
+  }
+
+
+}
